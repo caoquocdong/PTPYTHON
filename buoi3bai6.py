3f1c383972fbab3e5e275f2296f32f9bf28ee844
Cao Quoc Dong <caoquocdong604@gmail.com>
2026-04-21 09:44:41 +0700

    Bai dem word trong chuoi S

---
+++ buoi3bai6.py
@@ -0,0 +1,4 @@
+print ('Chuong Trinh chuoi trong Word')
+s = input('hay nhap chuoi S cua ban:')
+n = input("Nhap chuoi (world) can dem : ")
+print(f"So lan xuat hien cua chuoi '{n}' trong chuoi s la:", s.count(n, 0, len(s)))
